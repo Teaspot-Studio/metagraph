3386ec0b2aa2e338640c41d0718e043aa7485ed8
Anton Gushcha <ncrashed@gmail.com>
2017-02-17 02:59:37 +0300

    Add testcases

--- test/Tests.hs
+++ test/Tests.hs
@@ -16,13 +16,15 @@ properties = testGroup "Properties" []
 
 unitTests = testGroup "Unit tests" [
     testGroup "indexNode" [
-        testCase "Created nodes are queriable" $ isJust (indexNode (NodeId 0) testGraph1) @?= True
-      , testCase "Created nodes are queriable" $ isJust (indexNode (NodeId 1) testGraph1) @?= True
+        testCase "Created nodes are queriable1" $ isJust (indexNode (NodeId 0) testGraph1) @?= True
+      , testCase "Created nodes are queriable2" $ isJust (indexNode (NodeId 1) testGraph1) @?= True
+      , testCase "Created nodes are queriable3" $ isJust (indexNode (NodeId 1) testGraph2) @?= True
       , testCase "Non existed nodes are not queriable" $ isJust (indexNode (NodeId 2) testGraph1) @?= False
     ]
   , testGroup "indexEdge" [
-        testCase "Created edges are queriable" $ isJust (indexEdge (EdgeId 0) testGraph1) @?= True
-      , testCase "Created edges are queriable" $ isJust (indexEdge (EdgeId 1) testGraph1) @?= True
+        testCase "Created edges are queriable1" $ isJust (indexEdge (EdgeId 0) testGraph1) @?= True
+      , testCase "Created edges are queriable2" $ isJust (indexEdge (EdgeId 1) testGraph1) @?= True
+      , testCase "Created edges are queriable3" $ isJust (indexEdge (EdgeId 1) testGraph2) @?= True
       , testCase "Non existed edges are not queriable" $ isJust (indexEdge (EdgeId 2) testGraph1) @?= False
     ]
   ]
@@ -35,4 +37,20 @@ testGraph1 = buildMetaGraph $ do
   b <- newNode g Nothing "B"
   _ <- newEdge g Directed a b Nothing "1"
   _ <- newEdge g Directed b a Nothing "2"
+  return g
+
+-- | Non-planar metagraph
+testGraph2 :: MetaGraph String String
+testGraph2 = buildMetaGraph $ do
+  g  <- newMetaGraph
+  gA <- newMetaGraph
+  a1 <- newNode gA Nothing "A1"
+  a2 <- newNode gA Nothing "A2"
+  a3 <- newNode gA Nothing "A3"
+  _ <- newEdge gA Directed a1 a2 Nothing "a12"
+  _ <- newEdge gA Directed a2 a3 Nothing "a23"
+  _ <- newEdge gA Directed a3 a1 Nothing "a31"
+  a  <- newNode g (Just gA) "A"
+  b  <- newNode g Nothing "B"
+  _ <- newEdge g Directed b a1 Nothing "ba1"
   return g
